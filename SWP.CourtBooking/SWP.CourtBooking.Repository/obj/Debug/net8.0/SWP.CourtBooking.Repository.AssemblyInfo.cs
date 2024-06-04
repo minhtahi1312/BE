93c65c3674259724ea49461ae1e41af619a23d5e
khoanase172995 <khoanase172995@fpt.edu.vn>
2024-06-04 23:42:39 +0700

    delete fileContentIndex
    FileContentIndex folder may cause error when push code to github

--- SWP.CourtBooking/SWP.CourtBooking.Repository/obj/Debug/net8.0/SWP.CourtBooking.Repository.AssemblyInfo.cs
+++ SWP.CourtBooking/SWP.CourtBooking.Repository/obj/Debug/net8.0/SWP.CourtBooking.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWP.CourtBooking.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a8ffe909a58b3dd982541a3667f0eb8028343a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb7e032e6c887c84b7978cb30f3dfd00ef66f7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWP.CourtBooking.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWP.CourtBooking.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
