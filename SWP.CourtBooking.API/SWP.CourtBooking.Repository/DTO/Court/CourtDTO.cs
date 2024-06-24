86f220f39bed8de4fb1b422042132a5237c854ce
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-06-24 15:50:13 +0700

    Add CourtDTO

---
+++ SWP.CourtBooking.API/SWP.CourtBooking.Repository/DTO/Court/CourtDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP.CourtBooking.Repository.DTO.Court
+{
+    public class CourtDTO
+    {
+        public string CourtId { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string? Description { get; set; }
+        public string CourtClusterId { get; set; }
+    }
+}
