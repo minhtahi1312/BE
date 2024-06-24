09198b99388f314e16d20dda82b70b975ed2fe73
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-06-24 15:33:33 +0700

    Add BookingDTO

---
+++ SWP.CourtBooking.API/SWP.CourtBooking.Repository/DTO/Booking/BookingDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP.CourtBooking.Repository.DTO.Booking
+{
+    public class BookingDTO
+    {
+        public string BookingId { get; set; }
+        public string Code { get; set; }
+        public string Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CustomerId { get; set; }
+        public string CourtClusterId { get; set; }
+        public DateTime FromTime { get; set; }
+        public DateTime ToTime { get; set; }
+    }
+}
