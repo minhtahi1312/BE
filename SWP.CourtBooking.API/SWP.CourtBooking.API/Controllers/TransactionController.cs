a8cb482dd3604dfd2fa6e238278eb5da60680e41
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-07-04 17:23:09 +0700

    Add AddTransaction in TransactionController

--- SWP.CourtBooking.API/SWP.CourtBooking.API/Controllers/TransactionController.cs
+++ SWP.CourtBooking.API/SWP.CourtBooking.API/Controllers/TransactionController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SWP.CourtBooking.API.Models.TransactionModel;
+using SWP.CourtBooking.Repository.DTO.Court;
+using SWP.CourtBooking.Repository.DTO.Transaction;
+using SWP.CourtBooking.Repository.Models;
 using SWP.CourtBooking.Repository.UnitOfWork;
 
 namespace SWP.CourtBooking.API.Controllers
@@ -55,6 +58,49 @@ namespace SWP.CourtBooking.API.Controllers
             _unitOfWork.TransactionRepository.Update(existedTransaction);
             _unitOfWork.Save();
             return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddTransaction([FromBody] TransactionDTO transaction)
+        {
+            if (transaction == null)
+            {
+                return BadRequest("Transaction data is null");
+            }
+
+            // Validate BookingId presence
+            if (string.IsNullOrEmpty(transaction.BookingId))
+            {
+                return BadRequest("BookingId is required for the Transaction");
+            }
+
+            // Validate DepositId presence
+            if (string.IsNullOrEmpty(transaction.DepositId))
+            {
+                return BadRequest("DepositId is required for the Transaction");
+            }
+
+            // Validate WalletId presence
+            if (string.IsNullOrEmpty(transaction.WalletId))
+            {
+                return BadRequest("WalletId is required for the Transaction");
+            }
+
+            var addTransaction = new Transaction
+            {
+                TransactionId = transaction.TransactionId,
+                TotalSlot = transaction.TotalSlot,
+                TotalPrice = transaction.TotalPrice,
+                Status = transaction.Status,
+                BookingId = transaction.BookingId,
+                DepositId = transaction.DepositId,
+                WalletId = transaction.WalletId
+            };
+
+            _unitOfWork.TransactionRepository.Insert(addTransaction);
+            _unitOfWork.Save();
+
+            return Ok(transaction);
         }
     }
 }
