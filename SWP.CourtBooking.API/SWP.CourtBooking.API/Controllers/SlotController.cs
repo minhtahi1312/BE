776abe21b4b9829235129ce38b7a7aecf2104d0e
Lê Hoàng Phúc <147243750+PhucLHSE@users.noreply.github.com>
2024-07-04 16:56:23 +0700

    Add AddSlot in SlotController

--- SWP.CourtBooking.API/SWP.CourtBooking.API/Controllers/SlotController.cs
+++ SWP.CourtBooking.API/SWP.CourtBooking.API/Controllers/SlotController.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SWP.CourtBooking.API.Models.SlotModel;
+using SWP.CourtBooking.Repository.DTO.Deposit;
+using SWP.CourtBooking.Repository.DTO.Slot;
+using SWP.CourtBooking.Repository.Models;
 using SWP.CourtBooking.Repository.UnitOfWork;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace SWP.CourtBooking.API.Controllers
 {
@@ -53,6 +57,35 @@ namespace SWP.CourtBooking.API.Controllers
             _unitOfWork.SlotRepository.Update(existedSlot);
             _unitOfWork.Save();
             return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult AddSlot([FromBody] SlotDTO slot)
+        {
+            if (slot == null)
+            {
+                return BadRequest("Slot data is null");
+            }
+
+            // Validate CourtId presence
+            if (string.IsNullOrEmpty(slot.CourtId))
+            {
+                return BadRequest("CourtId is required for the Slot");
+            }
+
+            var addSlot= new Slot
+            {
+                SlotId = slot.SlotId,
+                Date = slot.Date,
+                StartTime = slot.StartTime,
+                EndTime = slot.EndTime,
+                CourtId = slot.CourtId
+            };
+
+            _unitOfWork.SlotRepository.Insert(addSlot);
+            _unitOfWork.Save();
+
+            return Ok(slot);
         }
     }
 }
